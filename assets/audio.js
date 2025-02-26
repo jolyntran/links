ce6da15916e567bee39ce836dc3813b0272e9e86
Jolyn Tran <tranj193@newschool.edu>
2025-02-26 07:26:35 -0500

    audio?

---
+++ assets/audio.js
@@ -0,0 +1,17 @@
+// AGAIN THE AUDIO HELPS TIE THE CONCEPT TOGETHER THAT THE BLOCKS ARE SONAR PINGS
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio
+// MAKES THE EXPLORE BUTTON PLAY OR RESTART THE AUDIO. IF IT'S PLAYING, IT'LL PAUSE AND RESET
+
+document.addEventListener("DOMContentLoaded", () => {
+    let exploreButtons = document.querySelectorAll(".explore-button");
+
+    exploreButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            let audio = button.nextElementSibling; 
+            if (audio.paused) {
+                audio.play();
+            } else {
+                audio.pause();
+                audio.currentTime = 0; 
+        });
+    });
